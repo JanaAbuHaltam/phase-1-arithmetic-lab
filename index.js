36b1991da5dc13c04120f7a06c394fe1462bf34c
Jana Salah <janaabuhaltam@gmail.com>
2023-09-24 15:25:51 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() * (Number.MAX_SAFE_INTEGER - 1)) + 1;
+
+// Task 3: Create a variable called mod set to an equation that will calculate the remainder of dividing variable num3 by num4; the remainder should be 4.
+let num3 = 29;
+let num4 = 5;
+let mod = num3 % num4;
+
+// Task 4: Create a variable called max that finds the highest number in a set; the value returned should be 20.
+let numbers = [10, 5, 20, 15];
+let max = Math.max(...numbers);
+
+// Print the results
+console.log("multiply:", multiply);
+console.log("random:", random);
+console.log("mod:", mod);
+console.log("max:", max);
+
+
+
+
+
+
+
